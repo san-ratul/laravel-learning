663bca3e80529ca7a8988414916d03af429f142e
shaheen2013 <san.ratul001@gmail.com>
2021-08-05 22:59:23 +0600

    Logout & User list

---
+++ resources/views/admin/contents/users/index.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.layouts.app')
+
+@section('section')
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users List</h3>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $key => $user)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
